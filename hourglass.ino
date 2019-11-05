81cbe4173304aff47b275ea03d89154c12bb625f
Philipp <github@phibography.de>
2019-11-06 00:50:44 +0100

    sand animation v1

--- hourglass.ino
+++ hourglass.ino
@@ -14,6 +14,8 @@ Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
 Adafruit_IS31FL3731 ledmatrix = Adafruit_IS31FL3731();
 
+#include <math.h>
+
 long startmillis;
 long duration = 1000 * 10;
 
@@ -39,8 +41,8 @@ void loop() {
   if (progress > 1.0) {
     progress = 1;
   }
-  Serial.println(progress);
-  delay(1000);
+  _drawSand(progress);
+  delay(100);
 }
 
 void _initAccel() {
@@ -93,7 +95,7 @@ void _initLed() {
 
 
 void _drawHourglassShape() {
-  int shapeBrightness = 72;
+  #define shapeBrightness 72
   ledmatrix.drawFastHLine(0, 0, 9, shapeBrightness);
   ledmatrix.drawFastVLine(0, 0, 3, shapeBrightness);
   ledmatrix.drawFastVLine(8, 0, 3, shapeBrightness);
@@ -104,4 +106,27 @@ void _drawHourglassShape() {
   ledmatrix.drawFastVLine(0, 13, 3, shapeBrightness);
   ledmatrix.drawFastVLine(8, 13, 3, shapeBrightness);
   ledmatrix.drawFastHLine(0, 15, 9, shapeBrightness);
+}
+
+void _drawSand(float progress) {
+  #define sandBrightness 32
+  #define EULER 2.718281828459045235360287471352
+  if (progress < 1) {
+    // upper
+    // triangle flows through neck -> exponentially decrease height
+    int upperTop = 3 + 4 * pow(EULER, 3 * progress - 3);
+    int upperLeft = upperTop / 2;
+    int upperRight = 8 - upperTop / 2;
+    ledmatrix.fillTriangle(upperLeft, upperTop, upperRight, upperTop, 4, 7, sandBrightness);
+    // lower
+    // triangle grows -> logarithmic increase height 
+    int lowerTop = 15 - 4 * ((1.0/2.0) * log(progress) + 1);
+    int lowerBottom = lowerTop + 4;
+    ledmatrix.fillTriangle(0, lowerBottom, 8, lowerBottom, 4, lowerTop, sandBrightness);
+    // stream
+    ledmatrix.drawLine(4, 8, 4, 15, sandBrightness);
+
+  } else {
+    ledmatrix.fillTriangle(0, 15, 8, 15, 4, 11, sandBrightness);
+  }
 }
